8dac03bd0f3a2aebf2c01671d963e9f78fea7d1d
Slavi Pantaleev <s.pantaleev@gmail.com>
2015-02-02 18:22:10 +0900

    Bump version

--- sijax/__init__.py
+++ sijax/__init__.py
@@ -6,7 +6,7 @@ from . import plugin
 from .core import Sijax
 
 __title__ = 'sijax'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 __author__ = 'Slavi Pantaleev'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2011 Slavi Pantaleev'
